bbcc871b0f7533f235961c262d1e5915092f5ba1
Nick Swetlin <nswetlin@ucsd.edu>
2024-10-13 20:58:43 -0700

    update description

--- fib.py
+++ fib.py
@@ -7,7 +7,7 @@ def F(n):
 
     memos = {0: 0, 1: 1}
     def fib(n):
-        '''Returns a list of the nth fibonacci numbers. When n = 0, returns 0. When n = 1, returns 1'''
+        '''Returns the nth fibonacci number, updates memos. When n = 0, returns 0. When n = 1, returns 1'''
 
         if n not in memos:
             memos[n] = fib(n - 1) + fib(n - 2)
